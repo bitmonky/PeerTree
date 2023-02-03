322bc7a837cea201510d136f24bec7772b46cb94
bitmonky <81542700+bitmonky@users.noreply.github.com>
2023-02-02 20:01:07 -0500

    Created The BitMonky PeerTree Wallet
    The wallet will allow payments for PeerTree services using the bitMonky BMGP token

---
+++ scripts/bitMonkyWSrv.js
@@ -0,0 +1,229 @@
+/****************************
+BitMonky Wallet Server
+****************************
+*/
+
+const webCon = require('http');
+const fs           = require('fs');
+const EC           = require('elliptic').ec;
+const ec           = new EC('secp256k1');
+const bitcoin      = require('bitcoinjs-lib');
+const crypto       = require('crypto');
+const port   = 80;
+const wfile  = 'keys/myBMGPWallet.key';
+const wconf  = 'keys/wallet.conf';
+
+class bitMonkyWSrv {
+  constructor(){
+    this.wallet = new bitMonkyWallet();
+    console.log(this.wallet);
+    this.allow = ["127.0.0.1"];
+    this.recPort = 1385;
+    this.readConfigFile();
+    this.srv = webCon.createServer( async (req, res) => {
+     console.log('safd?',req.url); 
+     if (req.url == '/keyGEN'){
+        res.writeHead(200);
+        res.end('KeyGEN not available on netCon');
+      }
+      else {
+        if (req.url.indexOf('/netREQ/msg=') == 0){
+          res.writeHead(200);
+          var msg = req.url.replace('/netREQ/msg=','');
+          msg = msg.replace(/\+/g,' ');
+          msg = decodeURI(msg);
+          msg = msg.replace(/%3A/g,':');
+          msg = msg.replace(/%2C/g,',');
+          msg = msg.replace(/\\%2F/g,'/');
+          var j = null;
+          try {
+            j = JSON.parse(msg);
+            console.log(j);
+            if (j.req == 'sendLoginToken'){
+              this.wallet.doSendLogin(j,res);
+              return;
+            } 
+            if (j.what == 'getNode'){
+              var report = this.sendReport();
+              res.end(report);
+            }
+            else 
+              res.end("No Handler Found For:\n\n "+JSON.stringify(j));
+          }
+          catch(err) {
+            //console.log("json parse error:",err);
+            res.end("JSON PARSE Error: \n\n"+msg+"\n\n"+err);
+          }
+        }
+        else {
+          res.setHeader("Content-Type", "text/html");
+          res.writeHead(200);
+          var htm = '<!doctype html><html class="pgHTML" lang="en"><head>';
+          htm += '<meta charset="utf-8"/><link rel="stylesheet" href="https://www.bitmonky.com/whzon/pc.css?v=1.0"/>';
+          htm += '<script src="https://www.bitmonky.com/bitMDis/pWalletJS.php"></script>';
+          htm += '</head><body class="pgBody" style="margin:5%;padding:1.5em;" onload="init();">';
+
+          htm += '<div align="right" ID="loginSpot"><input ID="loginBut" type="button" value=" BitMonky Login " onClick="doLogin()"/></div>';
+          htm += '<h1>Welcome To The BitMonky Wallet Server</h1>';
+          htm += '</body></html>';
+          res.end(htm);
+        }
+      }
+    });
+    this.srv.on('connection', (sock)=> {
+      console.log(sock.remoteAddress,this.allow);
+      if (this.allow.indexOf(sock.remoteAddress) < 0){
+        sock.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+      } 
+    });
+
+    this.srv.listen(port,'localhost');
+    console.log('bitMonky Wallet Server running at http://localhost:'+port);
+  }
+  readConfigFile(){
+     var conf = null;
+     try {conf =  fs.readFileSync(wconf);}
+     catch {console.log('no config file found');}
+     if (conf){
+       try {
+         conf = conf.toString();
+         const j = JSON.parse(conf);
+         this.recPort       = j.receptor.port;
+         this.allow         = j.receptor.allow;
+       }
+       catch(err) {
+         console.log('conf file not valid', err);
+         this.recPort = 1385;
+         this.allow = ["127.0.0.1"];
+       }
+     }
+  }
+};
+
+class bitMonkyWallet{
+   constructor(){
+      this.publicKey   = null;
+      this.privateKey  = null;
+      this.signingKey  = null;
+      this.openWallet();
+   }
+   calculateHash(txt) {
+      const crypto = require('crypto');
+      return crypto.createHash('sha256').update(txt).digest('hex');
+   }
+   signToken(token) {
+      const sig = this.signingKey.sign(calculateHash(token), 'base64');
+      const hexSig = sig.toDER('hex');
+      return hexSig;
+   }
+   openWallet(){
+      var keypair = null;
+      try {keypair =  fs.readFileSync(wfile);}
+      catch {console.log('no wallet file found');}
+      this.publicKey = null;
+      if (keypair){
+        try {
+          const pair = keypair.toString();
+          const j = JSON.parse(pair);
+          this.publicKey     = j.publicKey;
+          this.privateKey    = j.privateKey;
+          this.ownMUID       = j.ownMUID;
+          this.walletCipher  = j.walletCipher;
+          this.signingKey    = ec.keyFromPrivate(this.privateKey);
+        }
+        catch(err) {console.log('wallet file not valid', err);process.exit();
+        }
+      }
+      else {
+        const key = ec.genKeyPair();
+        this.publicKey = key.getPublic('hex');
+        this.privateKey = key.getPrivate('hex');
+        console.log('Generate a new wallet key pair and convert them to hex-strings');
+        var mkybc = bitcoin.payments.p2pkh({ pubkey: new Buffer.from(''+this.publicKey, 'hex') });
+        this.branchMUID = mkybc.address;
+
+        const pmc = ec.genKeyPair();
+        this.pmCipherKey  = pmc.getPublic('hex');
+
+        console.log('Generate a new wallet cipher key');
+        mkybc = bitcoin.payments.p2pkh({ pubkey: new Buffer.from(''+this.pmCipherKey, 'hex') });
+        this.shardCipher = mkybc.address;
+
+        var wallet = '{"ownMUID":"'+ this.branchMUID+'","publicKey":"' + this.publicKey + '","privateKey":"' + this.privateKey + '",';
+        wallet += '"walletCipher":"'+this.shardCipher+'"}';
+        console.log(wallet);
+        fs.writeFile(wfile, wallet, function (err) {
+          if (err) throw err;
+         //console.log('Wallet Created And Saved!');
+        });
+      }
+   }
+   signMsg(stok) {
+     const sig = this.signingKey.sign(this.calculateHash(stok), 'base64');
+     const hexSig = sig.toDER('hex');
+     return hexSig;
+   }
+   doSendLogin(j,res){
+     const stok = this.ownMUID+Date.now(); 	   
+     var msg = {
+       Address : this.ownMUID,
+       sesTok  : stok,
+       pubKey  : this.publicKey,
+       sesSig  : this.signMsg(stok),
+       action  : 'getToken'
+     }
+     this.sendPostRequest(msg,res);
+   }
+   handleResponse(data,res){
+     data.pMUID = this.ownMUID;
+     console.log(data);
+     if (res){
+       res.end(JSON.stringify(data));
+     }
+   }
+   sendPostRequest(msg,wres=null,endPoint='/whzon/gold/netWalletAPI.php'){
+      const https = require('https');
+
+      const data = JSON.stringify(msg);
+
+      const options = {
+        hostname : 'www.bitmonky.com',
+        port     : this.port,
+        path     : endPoint,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        } 
+      }
+
+      const req = https.request(options, res => {
+        var body = '';
+
+        res.on('data', (chunk)=>{
+          body = body + chunk;
+        });
+
+        res.on('end',()=>{
+          if (res.statusCode != 200) {
+            console.log("Api call failed with response code " + res.statusCode);
+          } 
+	  else {
+            this.handleResponse(JSON.parse(body),wres);
+          }
+        });
+      });
+
+      req.on('error', error => {
+         console.log(error);
+      });
+
+      req.write(data);
+      req.end();
+   } 
+};
+
+const myWallet = new bitMonkyWSrv();
+
+module.exports.bitMonkyWSrv = bitMonkyWSrv;
+
