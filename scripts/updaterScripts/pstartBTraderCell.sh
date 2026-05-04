bdd196119f89b52c579b7d1a783c5b3a318e0228
Guerrilla Coders Inc Headquarters <81542700+bitmonky@users.noreply.github.com>
2026-05-04 14:17:34 -0400

    small fix

--- scripts/updaterScripts/pstartBTraderCell.sh
+++ scripts/updaterScripts/pstartBTraderCell.sh
@@ -22,7 +22,9 @@ curl -s https://admin.bitmonky.com/bitMDis/peerCrypt.js         -o peerCrypt.js
 curl -s https://admin.bitmonky.com/bitMDis/mkyDatef.js          -o mkyDatef.js
 curl -s https://admin.bitmonky.com/bitMDis/networkWebConsole.js -o networkWebConsole.js
 curl -s https://admin.bitmonky.com/bitMDis/bitWebMoniter.js     -o bitWebMoniter.js
+curl -s https://admin.bitmonky.com/bitMDis/pstartBTraderCell.sh -o pstartBTraderCell.sh
 
 echo "CronoTreeCell files updated!"
 echo "Run with: pm2 start cronoTreeCell.js"
 
+node btraderOrganCell.js
